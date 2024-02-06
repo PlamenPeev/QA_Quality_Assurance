fcdb0213b731ad5f8d91ecb4d9081fe5ad3127ad
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2024-02-06 19:31:48 +0200

    Add files via upload

---
+++ 3.QA Automation/Back-End Test Automation/02.Data Formats JSON, and YAML, XML/Back-End Test Automation/02.Data Formats JSON, and YAML, XML/06.JS Fundamentals - Lab/11.Person info.js
@@ -0,0 +1,10 @@
+function personInfo(name1, name2, number){
+    let person = {
+        firstName : name1,
+        lastName : name2,
+        age : number
+    }
+
+   return person;
+}
+personInfo(ivan, ivanov, 13);
