8b4287c37e18321904758dadcd35dcd9abc635b2
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2024-02-11 11:09:24 +0200

    Add files via upload

---
+++ 3.QA Automation/Back-End Test Automation/02.Data Formats JSON, and YAML, XML/Back-End Test Automation/02.Data Formats JSON, and YAML, XML/07.JS Fundamentals - Exercises/11. Employees.js
@@ -0,0 +1,14 @@
+function emploeeys(names){
+
+      for(let name of names){
+        console.log(`Name: ${name} -- Personal Number: ${name.length}`);
+    }
+}
+
+emploeeys([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+    ]
+    );
